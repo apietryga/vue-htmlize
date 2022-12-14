52bceaf4fbec8f1fb37e36b8996236f489735161
apietryga <antek.pietryga@gmail.com>
2022-12-14 20:03:35 +0100

    mvp init

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-// exports.htmlizeRouter = function() {
-//   console.log("This is a message from the demo package");
-expors.htmlize = {
-  htmlizeRouter: ( router ) => {
+const fs = require('fs')
+const { resolve } = require('path');
 
-    console.log("IM IN MODULE")
-    console.log("AND I HAVE ROUTER")
-    console.log({ router })
+const htmlize = {
+  config : {
+    path : "public"
+  },
+  byRouter( router ){
+    htmlize.generateHTMLs( router.getRoutes() )
+  },
+  completeMissingFolders( route ){
+    for(const [ index, part ] of route.path.split("/").entries()){
+      if([''].includes(part) 
+      || index + 1 == route.path.split("/").length)
+        { continue }
 
+      if(!fs.existsSync(htmlize.config.path + '/' + part)){
+        fs.mkdirSync(htmlize.config.path + '/' + part)
+      }
+    }
+  },
+  async deleteUnusedFiles( pathes ){
+    const oldPathes = await htmlize.getPathMap(htmlize.config.path);
+    console.log({ oldPathes })
 
+  },
+  async getPathMap(dir){
+    const dirents = await fs.promises.readdir(dir, { withFileTypes: true })
+    const files = await Promise.all(dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name)
+      return dirent.isDirectory() ? htmlize.getPathMap(res) : res;
+    }));
+    return Array.prototype.concat(...files)
+  },
+  generateHTMLs( routes ){
+    const pathes = []
+    for(const route of routes){
+      htmlize.completeMissingFolders( route )
+      const path = htmlize.config.path + htmlize.serializeNames( route.path ) + '.html'
+      fs.writeFileSync( path, 'xdddx' )
+      pathes.push(path)
+    }
+    htmlize.deleteUnusedFiles(pathes)
+  },
+  serializeNames( name ){
+    if(name.includes("*")){ return '/404'}
+    if(name == "/" ){ return '/index'}
+    return name
   }
-  
-
 
 }
+exports.htmlize = htmlize 
