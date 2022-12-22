7cdfb05950eb9afee7c17fef8fdc38a32a5b857f
apietryga <antek.pietryga@gmail.com>
2022-12-22 18:00:55 +0100

    public

---
+++ index.js
@@ -0,0 +1,114 @@
+const fs = require('fs')
+const path = require('path');
+const { execSync } = require('child_process');
+
+const htmlize = {
+  config: {
+    dist: "public",
+    ignore: [ 'favicon.ico', '.htaccess' ],
+    missingFolders: [],
+    clean: false,
+    template: "index.html",
+  },
+  byRouter( router ){
+    htmlize.generateHTMLs( router.getRoutes() )
+  },
+  cmdRun( ){
+    const p = path.resolve("htmlize/routes.js");
+    const routes = require( p )
+    htmlize.generateHTMLs( routes )
+  },
+  completeMissingFolders( route ){
+    for(const [ index, part ] of route.path.split("/").entries()){
+      if([''].includes(part) 
+      || index + 1 == route.path.split("/").length){ 
+        continue 
+      }
+      const dPath = htmlize.config.dist + '/' + part
+      if(!htmlize.config.missingFolders.includes(dPath)){
+        htmlize.config.missingFolders.push( path.resolve( dPath ) )
+      }
+      if(!fs.existsSync( dPath )){
+        fs.mkdirSync( dPath )
+        return dPath
+      }
+    }
+  },
+  async deleteUnusedFiles( currentPathes ){
+    const allPathes = await htmlize.getPathMap(path.resolve( htmlize.config.dist ));
+    const unusedPathes = allPathes
+      .filter( p => ! currentPathes.includes( p ) )  // filter from current list
+      .filter( uPath => {   // filter from ignored list
+        for(const ig of htmlize.config.ignore){
+          const iPath = path.resolve(htmlize.config.dist, ig)
+          if(uPath.includes(iPath)){ return false }
+        }
+        return true
+      })
+
+    for( const uPath of unusedPathes ){
+      fs.lstatSync(uPath).isDirectory() ? fs.rmdirSync( uPath ) : fs.rmSync( uPath );
+    }    
+  },
+  // async deploy(){
+  deploy(){
+    try {
+      // await execSync("git", ["checkout", "--orphan", "public"]);
+      execSync("git checkout --orphan public");
+      console.log("Building started...");
+      execSync("npm run build");
+      const folderName = fs.existsSync("dist") ? "dist" : "build";
+      // execSync("git --work-tree " + folderName + " add --all");
+      execSync("git add .");
+      execSync("git --work-tree " + folderName + " commit -m public");
+      execSync("git push origin HEAD:public --force");
+      execSync("git checkout -f master");
+      execSync("git branch -D public");
+      console.log("Successfully deployed, check your settings");
+    } catch (e) {
+      console.log(e.message);
+      process.exit(1);
+    }
+
+  },
+  async getPathMap( dir ){
+    const dirents = await fs.promises.readdir(dir, { withFileTypes: true })
+    const files = await Promise.all(dirents.map((dirent) => {
+      const res = path.resolve(dir, dirent.name)
+      return dirent.isDirectory() ? htmlize.getPathMap(res) : res;
+    }));
+    return Array.prototype.concat(...files, dir)
+  },
+  generateHTMLContent( route ){
+    const template = fs.readFileSync(htmlize.config.template, 'utf-8')
+    const firstPart = template.split("<title>")[0]
+    const secondPart = template.split("</title>")[1]
+    const result = firstPart + '<title>' + route.name + '</title>' + secondPart;
+    return result
+  },
+  generateHTMLs( routes ){
+    htmlize.generateHtaccess( )
+    const pathes = htmlize.config.ignore.map( p => { return path.resolve( htmlize.config.dist, p ) })
+    pathes.push( path.resolve( htmlize.config.dist ) )
+    for(const route of routes){
+      htmlize.completeMissingFolders( route )
+      const sPath = path.resolve( htmlize.config.dist + htmlize.serializeNames( route.path ) + '.html' )
+      fs.writeFileSync( sPath, htmlize.generateHTMLContent(route) )
+      pathes.push(sPath)
+    }
+    pathes.push( ...htmlize.config.missingFolders )
+    if(this.config.clean){
+      htmlize.deleteUnusedFiles( pathes )
+    }
+  },
+  generateHtaccess( ){
+    fs.writeFileSync( htmlize.config.dist + '/.htaccess', `RewriteEngine On\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule ^([^\.]+)$ $1.php [NC,L]` )
+  },
+  serializeNames( name ){
+    if(name.includes("*")){ return '/404'}
+    if(name == "/" ){ return '/index'}
+    return name
+  }
+}
+
+module.exports = htmlize
---
+++ readme.md
@@ -0,0 +1,60 @@
+# vue-htmlize
+Make working OG Tags and SEO in pure Vue projects.
+Improve your SEO in SPA apps!
+
+## Get started
+### Install
+```bash
+npm i vue-htmlize
+```
+
+### Config
+- Make folder *htmlize* and create file *routes.js* in it.
+```js
+// You can copy paste here router.getRoutes() from vue router 
+
+module.exports = [
+  {
+      "path": "/projects/anadar",
+      "name": "anadar",
+      "meta": {},
+
+  ...
+```
+![configure routes](/img/routes.webp)
+
+
+### Run
+```bash
+node -e 'require(\"vue-htmlize\").cmdRun()'
+```
+
+<!-- You can add it before build in package.json: 
+```json
+"scripts": {
+  "build": "vite build",
+``` -->
+
+### Output
+
+![beforeafter](/img/beforeafter.webp)
+
+## Contributing
+Feel free to contribute at [GitHub](https://github.com/apietryga/vue-htmlize)
+
+
+<!-- ### EXPERIMENTAL AND TODO:
+Few options to setup it for your project
+```js
+// deletes unused files in public dir [ default : false ]
+htmlize.config.clean = true
+
+// files excluded from deletion [ default : '.htaccess', 'favicon.ico' ]
+htmlize.config.ignore = [ '.htaccess', 'favicon.ico', 'img' ]
+
+// directory for public files [ default: 'public']
+htmlize.config.dist = "public"
+
+// html template file to set global options (fonts, css) [ default: "index.html" ]
+htmlize.config.template = "index.html"
+``` -->
